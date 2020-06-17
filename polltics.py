022ee020c836f2b36e97be6c353369cb977edee2
김경서 <chloe@chloe-MacBook-Air.local>
2020-06-17 23:01:19 +0900

    submit file

--- polltics.py
+++ polltics.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
-import tensorflow as tf
-tf.debugging.set_log_device_placement(True)
-
-# 텐서 생성
-a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-c = tf.matmul(a, b)
-
-print(c)
-
-
 # In[10]:
 
 
